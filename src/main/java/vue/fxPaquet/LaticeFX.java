3596a37fbae8ae6eeb3307542cab361a7f25f88b
Julie Courty <julielanngoc10@gmail.com>
2025-05-14 17:38:14 +0200

    Amélioration de l'Ihm et du fichier css. Refactor des paquets

---
+++ src/main/java/vue/fxPaquet/LaticeFX.java
@@ -0,0 +1,25 @@
+package vue.fxPaquet;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class LaticeFX extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		BorderPane root = FXMLLoader.load((getClass().getResource("SB/FormeLatice.fxml")));
+		Scene scene = new Scene(root, 1300, 800);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Latice");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
---
+++ src/main/java/vue/fxPaquet/controleur/LaticeFXControleur.java
@@ -0,0 +1,5 @@
+package vue.fxPaquet.controleur;
+
+public class LaticeFXControleur {
+
+}
