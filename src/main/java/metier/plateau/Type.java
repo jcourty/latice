e2ecd12efeb156b26a573504e0d23957f56addbd
Kylian ROUSSEAU <kylian.rousseau@etu.unilim.fr>
2025-05-21 09:13:29 +0200

    modification enumeration Type pour régler un problème d'affichage

--- src/main/java/metier/plateau/Type.java
+++ src/main/java/metier/plateau/Type.java
@@ -1,8 +1,8 @@
 package metier.plateau;
 
 public enum Type {
-    SOLEIL("\u2600"), 
-    LUNE("\uD83C\uDF19"), 
+    SOLEIL(" \u2600 "), 
+    LUNE(" \uD83C\uDF19 "), 
     SIMPLE("simple");
 
     private String type;
